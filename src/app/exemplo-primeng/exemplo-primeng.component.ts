e12b8c56830d9370dffd77bb465a8a5f669efd68
Lucas Fischer <lucas.enrique.fischer@hotmail.com>
2024-09-05 19:09:00 -0300

    Criando Input para o usuário inserir a senha

--- src/app/exemplo-primeng/exemplo-primeng.component.ts
+++ src/app/exemplo-primeng/exemplo-primeng.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { PasswordModule } from 'primeng/password';
 import { RadioButtonModule } from 'primeng/radiobutton';
 
 
@@ -8,11 +9,17 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 @Component({
   selector: 'app-exemplo-primeng',
   standalone: true,
-  imports: [InputTextareaModule, FormsModule, RadioButtonModule],
+  imports: [
+    InputTextareaModule,
+    FormsModule,
+    RadioButtonModule,
+    PasswordModule 
+  ],
   templateUrl: './exemplo-primeng.component.html',
   styleUrl: './exemplo-primeng.component.css'
 })
 export class ExemploPrimengComponent {
   value!: string;
   ingredient!: string;
+  password!: string;
 }
