ea49ab239cf494c518fede64e287b99c607a3566
Lucas Fischer <lucas.enrique.fischer@hotmail.com>
2024-09-05 19:02:12 -0300

    Criando RadioButtons que permitirão o usuário escolher o sabor da pizza

--- src/app/exemplo-primeng/exemplo-primeng.component.ts
+++ src/app/exemplo-primeng/exemplo-primeng.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
+
 
 
 @Component({
   selector: 'app-exemplo-primeng',
   standalone: true,
-  imports: [InputTextareaModule, FormsModule],
+  imports: [InputTextareaModule, FormsModule, RadioButtonModule],
   templateUrl: './exemplo-primeng.component.html',
   styleUrl: './exemplo-primeng.component.css'
 })
 export class ExemploPrimengComponent {
   value!: string;
+  ingredient!: string;
 }
