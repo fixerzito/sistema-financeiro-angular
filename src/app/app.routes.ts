aebd06e744cf6cc7532c2b6796beb55cb82695c6
Lucas Fischer <lucas.enrique.fischer@hotmail.com>
2024-09-06 17:32:59 -0300

    Criando Cards para mostrar informações ao usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ExemploPrimengComponent } from './exemplo-primeng/exemplo-primeng.component';
+import { ExemploPrimeng2Component } from './exemplo-primeng-2/exemplo-primeng-2.component';
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'exemplos', component: ExemploPrimengComponent }
+    { path: 'exemplos', component: ExemploPrimengComponent },
+    { path: 'exemplos2', component: ExemploPrimeng2Component }
 ];
