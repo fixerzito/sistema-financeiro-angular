6ff9cd800f0821d7e7cb3b9045f585e853817f54
Fixerzito <lucas.enrique.fischer@hotmail.com>
2024-09-11 20:37:13 -0300

    Estilizando página de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [],
+=======
+  imports: [
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule 
+  ],
+>>>>>>> Stashed changes
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
