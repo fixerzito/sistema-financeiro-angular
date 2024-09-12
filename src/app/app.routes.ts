fd3a9555b1d5054f3a8602f7c67b6f24428f3715
Fixerzito <lucas.enrique.fischer@hotmail.com>
2024-09-12 16:49:43 -0300

    Criando tela de cadastro de cartão de crédito com possibilidade de vincular uma conta existente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { CadCatContaBancariaComponent } from './cad-cat-conta-bancaria/cad-cat-conta-bancaria.component';
 import { PageCadContaBancariaComponent } from './page-cad-conta-bancaria/page-cad-conta-bancaria.component';
 import { ListaCategoriaContaBancariaComponent } from './lista-categoria-conta-bancaria/lista-categoria-conta-bancaria.component';
+import { CadastroCartoesCreditoComponent } from './cadastro-cartoes-credito/cadastro-cartoes-credito.component';
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'cad-cat-conta-bancaria', component: CadCatContaBancariaComponent },
     { path: 'conta-bancaria', component: PageCadContaBancariaComponent },
-    { path: 'lista-categoria-conta-bancaria', component: ListaCategoriaContaBancariaComponent }
+    { path: 'lista-categoria-conta-bancaria', component: ListaCategoriaContaBancariaComponent },
+    { path: 'cad-cartao-credito', component: CadastroCartoesCreditoComponent }
 ];
