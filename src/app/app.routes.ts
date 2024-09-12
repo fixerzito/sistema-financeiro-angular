0b409763a09d350dbd596eee21f0a8707d0cf591
Fixerzito <lucas.enrique.fischer@hotmail.com>
2024-09-11 22:28:50 -0300

    Criando botão que ao ser clicado mostrará um Dialog com o cadastro da categoria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CadCatContaBancariaComponent } from './cad-cat-conta-bancaria/cad-cat-conta-bancaria.component';
 
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'cad-cat-conta-bancaria', component: CadCatContaBancariaComponent }
 ];
